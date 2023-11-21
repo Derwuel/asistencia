a30bd8cc79cf1a7057334f55b635361ab071247c
Marcos Hernandez <the.crazy.rabbit.000@gmail.com>
2023-11-21 00:50:44 -0300

    mitad de validacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +11,43 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 
 export class HomePage implements OnInit {
-
+  usuarios: any = [];
   formulariologin: FormGroup;
-  constructor(private fb: FormBuilder) {
+ 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient) {
     this.formulariologin = this.fb.group({
       "nombre" : new FormControl("", Validators.required),
       "password" : new FormControl("", Validators.required)
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getalumnos().subscribe(res=>{
+      console.log("res",res)
+      this.usuarios = res;
+    });
+  }
+
+  getalumnos(){
+    return this.http
+    .get("assets/files/gente.json")
+    .pipe(
+      map((res:any) =>{
+        return res.data;
+      })
+      )
+    }
+
+  async Ingresar() {
+    var f = this.formulariologin.value;
+    var u = this.usuarios;
+    if(u.nombre == f.nombre && u.password == f.password){
+      console.log("ingresado");
+    }else{
+      console.log("datos incorectos");
+    }
+  }
 
 }
